a66a9a1750109b258a3bb189d7d42251d950bcd1
Randy Houlahan <randy.houlahan@cbd.int>
2023-03-20 14:19:52 -0400

    locale comment

--- src/composables/locale.js
+++ src/composables/locale.js
@@ -6,9 +6,5 @@ export default () => scbdCMSLocale() || getUnLocale()
 function scbdCMSLocale(){
   if(typeof window === 'undefined') return ''
 
-  const pref = window.document.cookie.replace(/(?:(?:^|.*;\s*)Preferences\s*=\s*([^;]*).*$)|^.*$/, '$1')
-
-  if(pref) return (pref.replace('Locale=', ''))
-
-  return ''
+  return (/\bPreferences\s*=\s*Locale\s*=\s*(\w+)\b/.exec(window.document.cookie||'') || [])[1];
 }
